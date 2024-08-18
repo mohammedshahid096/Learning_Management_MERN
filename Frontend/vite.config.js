8c9113e889b7e1dc4e8f633df9f0965053662409
Mohammed shahid <www.mohdshahid303@gmail.com>
2024-08-18 23:01:44 +0530

    added sentry for detecting frontend errors

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,13 +1,22 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "shahid-34",
+    project: "javascript-react"
+  })],
+
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
   },
+
+  build: {
+    sourcemap: true
+  }
 });
